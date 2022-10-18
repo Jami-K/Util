59c6886f93c94f883f4b8a4cef8fc94e0148be59
Jami-K <87853545+Jami-K@users.noreply.github.com>
2022-10-18 21:55:41 +0900

    Update make_txt.py

--- make_txt.py
+++ make_txt.py
@@ -2,8 +2,11 @@ import os, random
 
 def make_list_txt():
     temp = []
-    label_dir = './temp'
+    label_dir = './jyb/220920/ok'
     label_list = os.listdir(label_dir)
+    label_list = [file for file in label_list if file.endswith(".jpg")]       # .jpg만 남김
+    label_list = [file for file in label_list if not file.startswith("._")]   # Dump data 삭제
+    print(label_list)
 
     for i in label_list:
         temp_list = i.split(".")
@@ -14,19 +17,18 @@ def make_list_txt():
 
     n = int(len(result)*0.8)
 
-    random.shuffle(result)
-    random.shuffle(result)
-
     basic_dir = os.path.abspath(label_dir)
 
     with open('Train.txt', 'w') as f:
         for i in result[:n]:
             data = "{}/{}.jpg\n".format(basic_dir, i)
+            #print("Train : {}".format(data))
             f.write(data)
 
     with open('Valid.txt', 'w') as f:
         for i in result[n:]:
             data = "{}/{}.jpg\n".format(basic_dir, i)
+            #print("Valid : {}".format(data))
             f.write(data)
 
 if __name__ == "__main__":
