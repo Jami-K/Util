b1fe4b6e53fe19e61873640991e70de435ce11e9
Jami-K <87853545+Jami-K@users.noreply.github.com>
2022-12-27 01:09:31 +0900

    Update rename.py

--- rename.py
+++ rename.py
@@ -1,13 +1,18 @@
 import os
- 
+
+dir_path = '/Users/kim/nongshim/작업용/멸치221212/OK'. #경로
+type_name = 'M'  #분류용 글자
+date = 2212120000   #이미지 촬영 일자
+
 def changeName(path, cName):
-    i = 22071100001
+    i = date
     for filename in os.listdir(path):
         print(path+filename, '=>', path+str(cName)+str(i)+'.jpg')
         os.rename(path+filename, path+str(cName)+str(i)+'.jpg')
         i += 1
- 
-changeName('/home/nongshim/tray/Database/2022-07-08/','')
+
+changeName(dir_path,type_name)
 
 # 간편하게 폴더 내 모든 파일 옮기는 방법
 # cp -r 폴더명/* Train
+# find ./ -type f -print0 | sort -zR | tail -zn +10000 | xargs -0 rm
