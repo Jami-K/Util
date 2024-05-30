5a1110849fa6cd9e3dd2b4fdfea0bb2262763cd8
Jami-K <87853545+Jami-K@users.noreply.github.com>
2024-05-30 10:14:50 +0900

    5분마다 저장되는 이미지 갯수 포함함

--- img_grab.py
+++ img_grab.py
@@ -1,21 +1,24 @@
-import cv2, os, shutil
+import cv2, os, shutil, time
 from multiprocessing import Process
 from pypylon import pylon
 
 class q2save:
     def main(self, camera_num):
-        #카메라 관련 설정
+        # 카메라 관련 설정
         camera_num = camera_num
         dirpath = './img_Grab/'
         window_name = 'Press Q to start saving Image / Press S to stop / ESC = Quit'
         self.camera_setting = None
         self.name = 0
         self.total_num = 0
-        self.operating = 0  #off
+        self.operating = 0  # off
 
         self.make_dir(dirpath)
         self.load_camera(camera_num)
         print("I'm Ready to Start!")
+
+        start_time = time.time()
+        saved_images = 0 
 
         while self.cameras.IsGrabbing():
             self.grabResult = self.cameras.RetrieveResult(50000, pylon.TimeoutHandling_ThrowException)
@@ -36,15 +39,21 @@ class q2save:
 
                 if k == ord('s'):
                     self.operating = 0
-                    
-                if self.opereating == 1:
+
+                if self.operating == 1 and saved_images < 10:
                     print("Save Image as {}.jpg".format(self.name))
                     self.total_num += 1
                     self.save_img(dirpath)
-                
+                    saved_images += 1
+
+                if saved_images == 10:
+                    if time.time() - start_time >= 300:  # 5분이 지났는지 확인
+                        saved_images = 0
+                        start_time = time.time()
+
                 if k == 27:
                     cv2.destroyAllWindows()
-                    print("\nYou got {} image sample..!\n".format(self.total_num)
+                    print("\nYou got {} image sample..!\n".format(self.total_num))
                     break
 
     def load_camera(self, camera_num):  # 카메라 설정 불러오기
@@ -57,7 +66,7 @@ class q2save:
             self.cam.Attach(tlFactory.CreateDevice(devices[camera_num]))
         self.cameras.Open()
 
-        if self.camera_setting != None:
+        if self.camera_setting is not None:
             pylon.FeaturePersistence.Load(self.camera_setting, self.cam.GetNodeMap(), True)
 
         self.cameras.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
@@ -66,7 +75,6 @@ class q2save:
         self.converter.OutputPixelFormat = pylon.PixelType_BGR8packed
         self.converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
 
-        
     def make_dir(self, dir_path):
         checking = str(dir_path) + '0.jpg'
         if os.path.exists(checking):
@@ -78,13 +86,13 @@ class q2save:
             else:
                 os.mkdir(dir_path)
         print("\nThe New Saving Folder is Ready..! \n")
-      
+
     def save_img(self, dirpath):
         dirpath_img = str(dirpath) + str(self.name) + '.jpg'
         cv2.imwrite(dirpath_img, self.img)
         self.name += 1
 
-        
+
 if __name__ == "__main__":
     a = q2save()
     a.main(0)
